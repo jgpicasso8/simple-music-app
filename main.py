998659c16cce5a507c8775f14126f0a02ecd2daf
Josue Gonzalez <66024359+jgpicasso8@users.noreply.github.com>
2020-08-27 10:53:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
+import doubly_linked_list as linked_list
 import sys
 import time
 import copy
 
 # Ensure there are 4 arguments
 if len(sys.argv) != 4:
-    raise ValueError("Please provide three file names: songs.txt, albums.txt, commands.txt")
+    raise ValueError("Please provide three file names.")
 
 sys.argv[0] = sys.argv[0][0:len(sys.argv[0]) - sys.argv[0][::-1].find("/")]
 inFile1 = sys.argv[0] + sys.argv[1]
@@ -17,102 +17,10 @@ print("\nThe files that will be used for input are {0}, {1}, and {2}"
 start = time.time()
 
 
-# Class Node is used for each Song title.
-class Node:
-    counter = 0
-
-    def __init__(self, value):
-        Node.counter += 1
-        self.value = value
-        self.next = None
-        self.prev = None
-
-
-# Class LinkedList is used for each album and once for the Playlist.
-class LinkedList:
-    def __init__(self):
-        self.head = self.tail = None
-        # The below count variables keep track of the commands in the command file.
-        self.forwardCount = self.backwardCount = self.playCount = self.beginCount = self.endCount = 0
-        # nowAt and playing keep track of where the player is at and what is actively playing.
-        self.nowAt = None
-        self.playing = None
-
-    # append takes a newNode and adds it to the end of the list or to the beginning if list is empty.
-    def append(self, newNode):
-        newNode.next = None
-        newNode.prev = None
-        if self.head is None:
-            self.head = newNode
-            self.tail = newNode
-        else:
-            self.tail.next = newNode
-            newNode.prev = self.tail
-            self.tail = newNode
-
-    # printList is is used to traverse and print the linked list.
-    def printList(self):
-        curNode = self.head
-        while curNode is not None:
-            print("-", curNode.value)
-            curNode = curNode.next
-
-    # finds node with matching key
-    def find(self, node_key):
-        self.nowAt = self.head
-        while self.nowAt is not None:
-            if self.nowAt.value == node_key:
-                return True
-            self.nowAt = self.nowAt.next
-        return False
-
-    # removes node that is found based on matching key
-    def remove(self, node_key):
-        self.find(node_key)
-        successor_node = self.nowAt.next
-        predecessor_node = self.nowAt.prev
-
-        if successor_node is not None:
-            successor_node.prev = predecessor_node
-        if predecessor_node is not None:
-            predecessor_node.next = successor_node
-
-        if self.nowAt is self.head:
-            self.head = successor_node
-        if self.nowAt is self.tail:
-            self.tail = predecessor_node
-
-    # command takes any command from the file and process it using the nowAt and playing.
-    # an if else structure facilitates this processing while incrementing the count variables for each command.
-    def command(self, request):
-        if request == 'Beginning':
-            self.nowAt = self.head
-            self.beginCount += 1
-            print("Now At:", self.nowAt.value)
-        elif request == 'Skip Forward':
-            self.nowAt = self.nowAt.next
-            self.forwardCount += 1
-            print("Now At:", self.nowAt.value)
-        elif request == 'Skip Backward':
-            self.nowAt = self.nowAt.prev
-            self.backwardCount += 1
-            print("Now At:", self.nowAt.value)
-        elif request == 'Play Track':
-            self.playing = self.nowAt
-            self.playCount += 1
-            print("Now Playing:", self.playing.value)
-        elif request == 'End':
-            self.nowAt = self.tail
-            self.endCount += 1
-            print("Now At:", self.nowAt.value)
-        else:
-            print("Unrecognised", request)
-
-
 # File 1: for each song in each album, there is a node made with prev/next pointers.
-album1List = LinkedList()
-album2List = LinkedList()
-album3List = LinkedList()
+album1List = linked_list.LinkedList()
+album2List = linked_list.LinkedList()
+album3List = linked_list.LinkedList()
 
 key1 = 0
 inputFile1 = open(sys.argv[1], "r")
@@ -124,11 +32,11 @@ for line in inputFile1:
         break
     if line != '\n':
         if key1 == 1:
-            album1List.append(Node(line.strip()))
+            album1List.append(linked_list.Node(line.strip()))
         elif key1 == 2:
-            album2List.append(Node(line.strip()))
+            album2List.append(linked_list.Node(line.strip()))
         elif key1 == 3:
-            album3List.append(Node(line.strip()))
+            album3List.append(linked_list.Node(line.strip()))
 inputFile1.close()
 
 print("Unordered doubly linked list Album #1")
@@ -199,7 +107,7 @@ print()
 # Each song is searched in its album and then appended to the playlist.
 inputFile1 = open(sys.argv[1], "r")
 key4 = False
-playList = LinkedList()
+playList = linked_list.LinkedList()
 for line in inputFile1:
     if line[:8] == 'Playlist':
         key4 = True
@@ -244,14 +152,14 @@ time = end - start
 print("\n**********************")
 print("***** Statistics *****")
 print("**********************")
-print("Total Songs Read:", Node.counter)
+print("Total Songs Read:", linked_list.Node.counter)
 forwardCount = album1List.forwardCount + album2List.forwardCount + album3List.forwardCount + playList.forwardCount
 print("Number of Skip Forward commands:", forwardCount)
 backwardCount = album1List.backwardCount + album2List.backwardCount + album3List.backwardCount + playList.backwardCount
 print("Number of Skip Backward commands:", backwardCount)
 playCount = album1List.playCount + album2List.playCount + album3List.playCount + playList.playCount
 print("Number of Play Next Track commands:", playCount)
-print("Number of Append commands:", Node.counter)
+print("Number of Append commands:", linked_list.Node.counter)
 beginCount = album1List.beginCount + album2List.beginCount + album3List.beginCount + playList.beginCount
 print("Number of Beginning Forward commands: ", beginCount)
 endCount = album1List.endCount + album2List.endCount + album3List.endCount + playList.endCount
